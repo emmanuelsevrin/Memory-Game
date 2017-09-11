61ff410164f4218d5629b69b802101f3e56201b0
Emmanuel Sevrin <emmanuel.sevrin@gmail.com>
2017-09-11 18:41:02 +0300

    Remove commentary at the end
    Remove pointless commentary at the end

--- js/app.js
+++ js/app.js
@@ -234,50 +234,7 @@ $(".card").on("click", function(){
 }
 )
 
-
-
-
 // get the game ready
 gamestart();
 
-//
-
-
-
-
-
-/*
-
-  var appendTens = document.getElementById("tens")
-  var appendSeconds = document.getElementById("seconds")
-  var buttonStart = document.getElementById('button-start');
-  var buttonStop = document.getElementById('button-stop');
-  var buttonReset = document.getElementById('button-reset');
-
-
-  var Interval ;
-
-  buttonStart.onclick = function() {
-     clearInterval(Interval);
-     Interval = setInterval(startTimer, 10);
-  }
-  
-
-    buttonStop.onclick = function() {
-       clearInterval(Interval);
-  }
-  
-
-  buttonReset.onclick = function() {
-     clearInterval(Interval);
-    tens = "00";
-  	seconds = "00";
-    appendTens.innerHTML = tens;
-  	appendSeconds.innerHTML = seconds;
-  }
-  
-}
-
-
-*/
 
